e483ded2624eaab5c4c9fda1222cd9fe2763782e
ConnorGrace11 <70079448+ConnorGrace11@users.noreply.github.com>
2022-06-01 02:06:14 -0400

    trying to add more data for model

---
+++ addStats.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+#Trying to add in data automatically so that it is less time consuming for any future data
+myData = {
+        
+        'Houston Texans': (72, 78),
+        'Jacksonville Jaguars': (75, 76),
+        'Kansas City Chiefs': (78, 96),
+        'Miami Dolphins': (91, 77),
+        'Denver Broncos': (86, 72),
+        'Green Bay Packers': (85, 91),
+        'Dallas Cowboys': (76, 88),
+        'Pittsburgh Steelers': (88, 74),
+        'Washington': (81, 76),
+        'New York Giants': (82, 72),
+        'Philadelphia Eagles': (72, 78),
+        'Tennessee Titans': (80, 87),
+        'Indianapolis Colts': (78, 75),
+        'Las Vegas Raiders': (75, 82),
+        'Las Angeles Chargers': (80, 82),
+        'Buffalo Bills': (82, 86),
+        'New England Patriots': (86, 78),
+        'New York Jets': (74, 69),
+        'Cincinnati Bengals': (74, 74),
+        'Cleveland Browns': (79, 89),
+        'Baltimore Ravens': (85, 84),
+        'Minnesota Vikings': (81, 77),
+        'Chicago Bears': (87, 73),
+        'Detroit Lions': (74, 75),
+        'New Orleans Saints': (83, 80),
+        'Carolina Panthers': (76, 71),
+        'Atlanta Falcons': (73, 79),
+        'Los Angeles Rams': (83, 81),
+        'Seattle Seahawks': (77, 85),
+        'Arizona Cardinals': (84, 83),
+        'San Francisco 49ers': (84, 80),
+        'Tampa Bay Buccaneers': (90, 92),
+}
+df = pd.read_csv('games.csv')
+df
+
+cols = ['Defense', 'Offense']
+df1 = (pd.DataFrame.from_dict(myData, orient='index', columns=cols)
+         .rename_axis('Visitors').reset_index())
+df1['Visitors']
+if df1['Visitors'].values[0]=="Houston Texans":
+    print("Hello")
+else:
+    print("Nope")
+
+#out = df.merge(df1, on='Visitors', how='left')
+#out.to_csv('gamescopy.csv', index=False)
